ddf1f19253133f857553e405d20ab2ea603c499d
Luis Concha <lu.conchaa@duocuc.cl>
2026-05-17 18:05:43 -0400

    Configuración final: Nginx proxy inverso, pipeline y red aislada

--- infra/etapa2/vpc.tf
+++ infra/etapa2/vpc.tf
@@ -25,12 +25,6 @@ resource "aws_subnet" "private_subnet" {
   tags = { Name = "${var.project_name}-private_subnet" }
 }
 
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
-
-  tags = { Name = "${var.project_name}-private-rt" }
-}
-
 resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
@@ -45,8 +39,16 @@ resource "aws_route_table" "private_rt" {
   tags = { Name = "${var.project_name}.private-rt"}
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+  subnet_id      = aws_subnet.public_subnet.id  
   route_table_id = aws_route_table.public.id
 }
 
