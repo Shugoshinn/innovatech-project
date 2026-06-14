441a45fabe0112b0bff3abc5a2d447e1c93aa0fa
Luis Concha <lu.conchaa@duocuc.cl>
2026-06-14 14:42:51 -0400

    Fix: error en las rutas de nginx correjidas

--- front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("/api/despachos", {
+      .get("/api/v1/despachos", {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
