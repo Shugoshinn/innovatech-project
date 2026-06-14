2e6433e45f7f4d1bbe45d9fb192ac095796986bd
Luis Concha <lu.conchaa@duocuc.cl>
2026-06-14 15:29:37 -0400

    Feat: Data seeding con data.sql y properties actualizados

---
+++ back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) VALUES 
+(1, '2026-06-14', 'HX-LK-89', 1, 1, 'Av. Recoleta 1234, Santiago', 45000, false),
+(2, '2026-06-15', 'KL-MN-34', 0, 2, 'Providencia 456, Santiago', 120000, false),
+(3, '2026-06-16', 'AB-CD-12', 2, 3, 'Alameda 980, Santiago', 15000, true);
---
+++ back-Ventas_SpringBoot/Springboot-API-REST/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO venta (id_venta, fecha_compra, direccion_compra, valor_compra, despacho_generado) VALUES 
+(1, '2026-06-12', 'Av. Recoleta 1234, Santiago', 45000, false),
+(2, '2026-06-13', 'Providencia 456, Santiago', 120000, false),
+(3, '2026-06-14', 'Alameda 980, Santiago', 15000, false);
